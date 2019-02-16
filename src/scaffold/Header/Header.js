9932c01e0339e67c6da326810d904d05f66ebd47
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2019-02-16 14:05:33 +0000

    Removed state

--- src/scaffold/Header/Header.js
+++ src/scaffold/Header/Header.js
@@ -8,28 +8,22 @@ const RightComponent = () => {
 };
 
 class Applications extends DashboardItem {
-    constructor(props) {
-        super(props);
-
-        this.state = { test: 1 };
-        this.dashboardItemName = 'applications';
-    }
+    
     render() {
         const { query } = this.props;
-        const { test } = this.state;
 
         return <div className="applications-container">
-            This will be the list of installed applications! Query: {query} {test}
-            <button onClick={() => this.setState({ test: test + 1 })}>Up state</button>
+            This will be the list of installed applications! Query: {query}
         </div>;
     }
+
 }
 
 export class Header extends Component {
     render() {
         return <header>
             <DashboardMenu
-                left={[{ headerLabel: 'Applications', bodyItem: ({ query }) => <Applications query={query} /> }, 'Menu Item 2']}
+                left={[{ headerLabel: 'Applications', bodyItem: Applications, name: 'applications' }, 'Menu Item 2']}
                 center='Dashboard'
                 right={<RightComponent />}
             />
